c6071e866e94a1468d909891a5598adf81e65076
Admin <Admin@10.0.0.4>
2022-07-05 21:08:32 +0300

    adsd

---
+++ src/test/java/Projact/LaunchChrome.java
@@ -0,0 +1,23 @@
+package Projact;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) {
+	
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.wallashops.co.il/");
+	
+
+		
+		
+
+	}
+
+}
